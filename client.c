276a6958ebeac9a05720bb15b9e52fa6b6dfe871
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-10-17 21:12:43 -0300

    Update client.c

--- client.c
+++ client.c
@@ -6,11 +6,12 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 13:44:42 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/10/04 18:58:41 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/10/17 21:09:41 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include <unistd.h>
+#include <signal.h>
 
 int	ft_atoi(const char *nptr)
 {
