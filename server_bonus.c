fe5d06a313d97da3c549eee38f123d16b19761a5
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-10-21 20:05:25 -0300

    Update server_bonus.c

--- server_bonus.c
+++ server_bonus.c
@@ -6,12 +6,11 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 13:52:06 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/10/17 21:09:19 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/10/21 19:59:17 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <signal.h>
+#include "minitalk_bonus.h"
 
 void	ft_putnbr(int n)
 {
